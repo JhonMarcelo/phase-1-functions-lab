61bab0d761835db96f317e1894291645919c322e
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-03-19 22:17:02 -0400

    Complete Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber){
+let numberOfBlocksAway = blockNumber - 42;
+
+if(numberOfBlocksAway > 0){
+    return numberOfBlocksAway;
+} else {
+    return Math.abs(numberOfBlocksAway)
+}
+}
+
+function distanceFromHqInFeet(someValue){
+    let numberOfBlocks = distanceFromHqInBlocks(someValue);
+    return numberOfBlocks * 264
+}
+
+function distanceTravelledInFeet(block1,block2){
+    let blockDistance = block2 - block1;
+    if (blockDistance>0){
+        return blockDistance*264;
+    } else {
+        return (Math.abs(blockDistance))*264;
+    }
+
+}
+
+function calculatesFarePrice(start,destinatin){
+let distanceInFeet = distanceTravelledInFeet(start,destinatin);
+if (distanceInFeet <= 400){
+    return 0;
+} else if (distanceInFeet > 2500){
+    return "cannot travel that far";
+} else if ((distanceInFeet > 400) && (distanceInFeet < 2000)){
+    return (distanceInFeet-400)*.02
+} else if (distanceInFeet > 2000){
+    return 25;
+}
+
+}
